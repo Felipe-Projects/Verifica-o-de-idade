d609941b8681b1c4191ff467d60f92c52318ca4e
Felipe-Projects <82413144+Felipe-Projects@users.noreply.github.com>
2021-05-10 15:49:06 -0300

    Add files via upload

---
+++ Eleitor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Eleitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            int year = DateTime.Now.Year;
+            Console.WriteLine("Em que ano você nasceu?");
+            int datanascimento = int.Parse(Console.ReadLine());
+
+            int idade = year - datanascimento;
+
+            if (idade < 18)
+            {
+                Console.WriteLine("Você não poderá votar este ano, pois não tem a idade mínima necessária");
+            }
+            else
+            {
+                Console.WriteLine("Você pode votar");
+            }
+
+
+        }
+
+
+    }
+}
